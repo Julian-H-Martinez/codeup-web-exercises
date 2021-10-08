3abff62b07a7a35bf4ebfdfa810d7d7b70fc1fab
Julian Martinez <julianmartinez1830@gmail.com>
2021-10-08 14:20:09 -0500

    feat: added/completed pay per hour exercise

--- js/external.js
+++ js/external.js
@@ -10,19 +10,38 @@ alert(`Really? ${favColor} is my favorite color too!!!`);
 
 /**
  *
- Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour. Google pays $400, Amazon $380, and Facebook $350. How much will you receive in payment for this week? You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
  A student can be enrolled in a class only if the class is not full and the class schedule does not conflict with her current schedule.
  A product offer can be applied only if a person buys more than 2 items, and the offer has not expired. Premium members do not need to buy a specific amount of products.
  */
 /**
  * You have rented some movies for your kids: The little mermaid (for 3 days), Brother Bear (for 5 days, they love it), and Hercules (1 day, you don't know yet if they're going to like it). If price for a movie per day is $3, how much will you have to pay?
  */
-alert("Cost per day for rental is $3.")
-var littleMermaidDays = prompt("How many days would you like to rent The Little Mermaid?");
-var brotherBearDays = prompt("How many days would you like to rent Brother Bear?");
-var herculesDays = prompt("How many days would you like to rent Hercules?");
+// alert("Cost per day for rental is $3.")
+// var littleMermaidDays = prompt("How many days would you like to rent The Little Mermaid?");
+// var brotherBearDays = prompt("How many days would you like to rent Brother Bear?");
+// var herculesDays = prompt("How many days would you like to rent Hercules?");
+//
+// var totalDays = parseFloat(littleMermaidDays) + parseFloat(brotherBearDays) + parseFloat(herculesDays);
+// var totalCost = totalDays * 3;
+//
+// alert(`Thank you for your rental. Your total cost is S${totalCost}. Be kind and rewind!`)
 
-var totalDays = parseFloat(littleMermaidDays) + parseFloat(brotherBearDays) + parseFloat(herculesDays);
-var totalCost = totalDays * 3;
+/**
+ *  Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour. Google pays $400, Amazon $380, and Facebook $350. How much will you receive in payment for this week? You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
+ */
+alert("Let's see how much you earned this week.");
 
-alert(`Thank you for your rental. Your total cost is S${totalCost}. Be kind and rewind!`)
+var googleHours = prompt("How many hours did you work at Google?");
+var googlePay = parseFloat(googleHours) * 400;
+alert(`Your pay for Google is $${googlePay}.`);
+
+var amazonHours = prompt("How many hours did you work at Amazon?");
+var amazonPay = parseFloat(amazonHours) * 380;
+alert(`Your pay for Amazon is $${amazonPay}.`);
+
+var facebookHours = prompt("How many hours did you work at Facebook?");
+var facebookPay = parseFloat(facebookHours) * 350;
+alert(`Your pay for Facebook is $${facebookPay}.`);
+
+var totalPay = googlePay + amazonPay + facebookPay;
+alert(`Your total pay for all work done is $${totalPay}! Congrats!!!`);
