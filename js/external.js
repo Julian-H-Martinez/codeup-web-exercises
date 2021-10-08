9e96b8fba97ae52b20bd9e6b81f65c9df0008e69
Julian Martinez <julianmartinez1830@gmail.com>
2021-10-08 13:46:22 -0500

    feat: added h1 to external html; added/completed movie rental scenario.

--- js/external.js
+++ js/external.js
@@ -1,9 +1,28 @@
 "use strict";
 
 console.log("Hello from External JavaScript");
-
+// Completed first part of exercise //
+/**
 alert("Welcome to my Website!");
-
 var favColor = prompt("What's your favorite color?");
-
 alert(`Really? ${favColor} is my favorite color too!!!`);
+ */
+
+/**
+ *
+ Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour. Google pays $400, Amazon $380, and Facebook $350. How much will you receive in payment for this week? You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
+ A student can be enrolled in a class only if the class is not full and the class schedule does not conflict with her current schedule.
+ A product offer can be applied only if a person buys more than 2 items, and the offer has not expired. Premium members do not need to buy a specific amount of products.
+ */
+/**
+ * You have rented some movies for your kids: The little mermaid (for 3 days), Brother Bear (for 5 days, they love it), and Hercules (1 day, you don't know yet if they're going to like it). If price for a movie per day is $3, how much will you have to pay?
+ */
+alert("Cost per day for rental is $3.")
+var littleMermaidDays = prompt("How many days would you like to rent The Little Mermaid?");
+var brotherBearDays = prompt("How many days would you like to rent Brother Bear?");
+var herculesDays = prompt("How many days would you like to rent Hercules?");
+
+var totalDays = parseFloat(littleMermaidDays) + parseFloat(brotherBearDays) + parseFloat(herculesDays);
+var totalCost = totalDays * 3;
+
+alert(`Thank you for your rental. Your total cost is S${totalCost}. Be kind and rewind!`)
