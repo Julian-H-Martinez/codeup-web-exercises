df52b5ab79e1aedfe0ac783e04d49e430409a0e2
Julian Martinez <julianmartinez1830@gmail.com>
2021-10-08 14:34:13 -0500

    feat: added/completed enrollment exercise

--- js/external.js
+++ js/external.js
@@ -10,7 +10,6 @@ alert(`Really? ${favColor} is my favorite color too!!!`);
 
 /**
  *
- A student can be enrolled in a class only if the class is not full and the class schedule does not conflict with her current schedule.
  A product offer can be applied only if a person buys more than 2 items, and the offer has not expired. Premium members do not need to buy a specific amount of products.
  */
 /**
@@ -29,19 +28,29 @@ alert(`Really? ${favColor} is my favorite color too!!!`);
 /**
  *  Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour. Google pays $400, Amazon $380, and Facebook $350. How much will you receive in payment for this week? You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
  */
-alert("Let's see how much you earned this week.");
+// alert("Let's see how much you earned this week.");
+//
+// var googleHours = prompt("How many hours did you work at Google?");
+// var googlePay = parseFloat(googleHours) * 400;
+// alert(`Your pay for Google is $${googlePay}.`);
+//
+// var amazonHours = prompt("How many hours did you work at Amazon?");
+// var amazonPay = parseFloat(amazonHours) * 380;
+// alert(`Your pay for Amazon is $${amazonPay}.`);
+//
+// var facebookHours = prompt("How many hours did you work at Facebook?");
+// var facebookPay = parseFloat(facebookHours) * 350;
+// alert(`Your pay for Facebook is $${facebookPay}.`);
+//
+// var totalPay = googlePay + amazonPay + facebookPay;
+// alert(`Your total pay for all work done is $${totalPay}! Congrats!!!`);
 
-var googleHours = prompt("How many hours did you work at Google?");
-var googlePay = parseFloat(googleHours) * 400;
-alert(`Your pay for Google is $${googlePay}.`);
+/**
+ *  A student can be enrolled in a class only if the class is not full and the class schedule does not conflict with her current schedule.
+ */
 
-var amazonHours = prompt("How many hours did you work at Amazon?");
-var amazonPay = parseFloat(amazonHours) * 380;
-alert(`Your pay for Amazon is $${amazonPay}.`);
+var classFull = confirm("Is the class full?");
+var scheduleConflict = confirm("The class is on MWF, do you have any classes during this time?");
+var canEnroll = !classFull && !scheduleConflict;
 
-var facebookHours = prompt("How many hours did you work at Facebook?");
-var facebookPay = parseFloat(facebookHours) * 350;
-alert(`Your pay for Facebook is $${facebookPay}.`);
-
-var totalPay = googlePay + amazonPay + facebookPay;
-alert(`Your total pay for all work done is $${totalPay}! Congrats!!!`);
+alert(`You can enroll... ${canEnroll}!`);
