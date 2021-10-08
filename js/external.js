ee8e3aef223713c7e6ed0db1d33ea1761b4bd9a2
Julian Martinez <julianmartinez1830@gmail.com>
2021-10-08 12:15:30 -0500

    feat: added 2 alerts and a prompt to external.js

--- js/external.js
+++ js/external.js
@@ -1,3 +1,9 @@
 "use strict";
 
 console.log("Hello from External JavaScript");
+
+alert("Welcome to my Website!");
+
+var favColor = prompt("What's your favorite color?");
+
+alert(`Really? ${favColor} is my favorite color too!!!`);
