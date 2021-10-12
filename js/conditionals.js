2978fc29eb56d01c3fc423ddec2e0cf019f3d1e2
Julian Martinez <julianmartinez1830@gmail.com>
2021-10-12 12:10:45 -0500

    feat: added IFFE to functions and conditionals js

--- js/conditionals.js
+++ js/conditionals.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /* ########################################################################## */
-
+(function(){
 /**
  * TODO:
  * Create a function named `analyzeColor` that accepts a string that is a color
@@ -89,35 +89,35 @@
  * Test your function by passing it various values and checking for the expected
  * return value.
  */
-function calculateTotal (luckyNum, totalAmt){
-    var discountedPrice;
-    if(luckyNum === 5){
-        discountedPrice = 0;
-        return `Your price after discount is $${discountedPrice}.00! Congratulations!!!`;
-    }else if(luckyNum === 4){
-        discountedPrice = totalAmt - (.50*totalAmt);
-        return `Your price after discount is $${discountedPrice.toFixed(2)}!`;
-    }else if(luckyNum === 3){
-        discountedPrice = totalAmt - (.35*totalAmt);
-        return `Your price after discount is $${discountedPrice.toFixed(2)}!`;
-    }else if(luckyNum === 2){
-        discountedPrice = totalAmt - (.25*totalAmt);
-        return `Your price after discount is $${discountedPrice.toFixed(2)}!`;
-    }else if(luckyNum === 1){
-        discountedPrice = totalAmt - (.10*totalAmt);
-        return `Your price after discount is $${discountedPrice.toFixed(2)}!`;
-    }else{
-        discountedPrice = totalAmt;
-        return `Awe shucks, no discount this time. Your total is $${discountedPrice}.`
-    }
-}
-
-console.log(calculateTotal(5, 100)); // returns 100
-console.log(calculateTotal(4, 100)); // returns 100
-console.log(calculateTotal(3, 100)); // returns 100
-console.log(calculateTotal(2, 100)); // returns 100
-console.log(calculateTotal(1, 100)); // returns 100
-console.log(calculateTotal(0, 100)); // returns 100
+// function calculateTotal (luckyNum, totalAmt){
+//     var discountedPrice;
+//     if(luckyNum === 5){
+//         discountedPrice = 0;
+//         return `Your price after discount is $${discountedPrice}.00! Congratulations!!!`;
+//     }else if(luckyNum === 4){
+//         discountedPrice = totalAmt - (.50*totalAmt);
+//         return `Your price after discount is $${discountedPrice.toFixed(2)}!`;
+//     }else if(luckyNum === 3){
+//         discountedPrice = totalAmt - (.35*totalAmt);
+//         return `Your price after discount is $${discountedPrice.toFixed(2)}!`;
+//     }else if(luckyNum === 2){
+//         discountedPrice = totalAmt - (.25*totalAmt);
+//         return `Your price after discount is $${discountedPrice.toFixed(2)}!`;
+//     }else if(luckyNum === 1){
+//         discountedPrice = totalAmt - (.10*totalAmt);
+//         return `Your price after discount is $${discountedPrice.toFixed(2)}!`;
+//     }else{
+//         discountedPrice = totalAmt;
+//         return `Awe shucks, no discount this time. Your total is $${discountedPrice}.`
+//     }
+// }
+//
+// console.log(calculateTotal(5, 100)); // returns 100
+// console.log(calculateTotal(4, 100)); // returns 100
+// console.log(calculateTotal(3, 100)); // returns 100
+// console.log(calculateTotal(2, 100)); // returns 100
+// console.log(calculateTotal(1, 100)); // returns 100
+// console.log(calculateTotal(0, 100)); // returns 100
 /**
  * TODO:
  * Uncomment the line below to generate a random number between 0 and 6.
@@ -144,3 +144,4 @@ console.log(calculateTotal(0, 100)); // returns 100
  * Can you refactor your code to use functions?
  * HINT: The way we prompt for a value could be improved
  */
+})();
