a4370288d4c113f4ba127736ed181c3ac3349cd5
Julian Martinez <julianmartinez1830@gmail.com>
2021-10-13 09:52:52 -0500

    feat: added 2 files (html/js) for morning warm-ups; completed 10.13warm-up

---
+++ js/warm-ups.js
@@ -0,0 +1,13 @@
+"use strict";
+(function (){
+    //  Write a function that returns the number 7
+    function isSeven(input){
+        return 7;
+    }
+    console.log(isSeven());
+
+    //  boolean of false action will not happen
+    if(false){
+        console.log('Am I seen?');
+    }
+}) ();
