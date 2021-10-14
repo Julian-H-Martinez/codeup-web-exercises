d7513d28a8819c5cd55c4f5ac3189c64220a388d
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2021-10-14 09:29:32 -0500

    testing push

--- js/warm-ups.js
+++ js/warm-ups.js
@@ -1,13 +1,28 @@
 "use strict";
 (function (){
     //  Write a function that returns the number 7
-    function isSeven(input){
-        return 7;
-    }
-    console.log(isSeven());
+    // function isSeven(input){
+    //     return 7;
+    // }
+    // console.log(isSeven());
+    //
+    // //  boolean of false action will not happen
+    // if(false){
+    //     console.log('Am I seen?');
+    // }
 
-    //  boolean of false action will not happen
-    if(false){
-        console.log('Am I seen?');
-    }
+    /**
+     * The following code will set the variable hadBreakfast to a random boolean value.
+     * Copy and paste the following code into your warmups JS file:
+     var hadBreakfast = Boolean(Math.round(Math.random()));
+     Write a ternary expression to set a variable of hadBreakfastMessage to a string expressing whether or
+     not you have had breakfast. You will use the variable hadBreakfast as your condition.
+     * @type {boolean}
+     */
+    var hadBreakfast = Boolean(Math.round(Math.random()));
+
+    var hadBreadfastMessage = hadBreakfast ? 'I had a steak omelette with vegetables and salsa!!! Delish!' :
+        'No, I fast until lunch time. Or at least I try to. Hahahahaha!!!';
+    console.log(hadBreakfast);
+    console.log(hadBreadfastMessage);
 }) ();
