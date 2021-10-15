e772f7d57b5182cb648edc8a81361a25f9905cac
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2021-10-15 09:08:42 -0500

    typeCheck() function warm-up completed

--- js/warm-ups.js
+++ js/warm-ups.js
@@ -19,10 +19,32 @@
      not you have had breakfast. You will use the variable hadBreakfast as your condition.
      * @type {boolean}
      */
-    var hadBreakfast = Boolean(Math.round(Math.random()));
+    // var hadBreakfast = Boolean(Math.round(Math.random()));
+    //
+    // var hadBreadfastMessage = hadBreakfast ? 'I had a steak omelette with vegetables and salsa!!! Delish!' :
+    //     'No, I fast until lunch time. Or at least I try to. Hahahahaha!!!';
+    // console.log(hadBreakfast);
+    // console.log(hadBreadfastMessage);
 
-    var hadBreadfastMessage = hadBreakfast ? 'I had a steak omelette with vegetables and salsa!!! Delish!' :
-        'No, I fast until lunch time. Or at least I try to. Hahahahaha!!!';
-    console.log(hadBreakfast);
-    console.log(hadBreadfastMessage);
+    /**
+     * Write a function called ‘typeCheck’ that takes an input and returns a string of the data type entered.
+     * If the data type is a string, return the string “Haha, it’s a string of string!”
+     *
+     Tests:
+     typeCheck(‘’)
+     typeCheck(isNaN());
+     typeCheck(!false * 3);
+     typeCheck(4 + ‘4’);
+     */
+    function typeCheck(input){
+        if(typeof input === 'string'){
+            return `Ha Ha, this is a string!`
+        }else{
+            return typeof input;
+        }
+    }
+    console.log(typeCheck(''));
+    console.log(typeCheck(isNaN()));
+    console.log(typeCheck(!false * 3));
+    console.log(typeCheck(4 + '4'));
 }) ();
