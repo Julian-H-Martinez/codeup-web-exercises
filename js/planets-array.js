ce060adde304c1e9967e63d6bf73551844163b26
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2021-10-18 15:46:49 -0500

    js: TODO5 completed

--- js/planets-array.js
+++ js/planets-array.js
@@ -35,6 +35,9 @@
     console.log(planets);
 
     console.log('Finding and logging the index of "Earth" in the planets array.');
+    // var indexOfEarth = planets.indexOf('Earth');
+    // console.log(indexOfEarth);
+    console.log(planets.indexOf('Earth'));
 
     console.log("Reversing the order of the planets array.");
     console.log(planets);
