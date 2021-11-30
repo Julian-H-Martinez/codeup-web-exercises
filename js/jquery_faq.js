4a9b74ac0dfb7fdec67c99f3b3ac66bab248970a
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2021-11-29 19:01:30 -0600

    added btn w/click event toggle bkg of last li in each ul

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     $('dd').addClass('invisible');
 
     //  TODO: add click event for button to flip from hidden to not-hidden on descriptions
-    $('#hide').click(function(e){
+    $('#hide').click(function (e) {
         e.preventDefault();
         $('dd').toggleClass('invisible')
         $('#hide').html('Hide Description')
@@ -13,10 +13,19 @@ $(document).ready(function () {
     //  Bonus:
     //  TODO: add click event to highlight each 'dt'
     //  NOTE: $(this) key, will allow you to click on individual elements
-    $('dt').click(function(e){
+    $('dt').click(function (e) {
         e.preventDefault();
         $(this).toggleClass('highligthed')
     })
 
+    //  TODO: add click event to button so that each li item background is yellow
+    $('#yellowBkg').click(function () {
+        // e.preventDefault();
+        $('ul').each(function(index){
+            if(index >= 0){
+                $(this).children().last().toggleClass('highligthed2')
+            }
+        })
+    })
 
 })
