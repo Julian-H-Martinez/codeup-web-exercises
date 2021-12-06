60d4b4033073d8e2cc49ce1297ef3fb3de688f12
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2021-12-05 22:56:36 -0600

    added map to html and js; added main section to html

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,5 +1,6 @@
 "use strict";
 $(document).ready(function(){
+    mapboxgl.accessToken = MAPBOX_KEY;
     $.get("http://api.openweathermap.org/data/2.5/onecall", {
         APPID: WEATHER_KEY,
         lat:    29.423017,
@@ -11,8 +12,19 @@ $(document).ready(function(){
         // console.log('A step further - information for tomorrow: ', data.daily[1]);
     });
 
+//    MAP(s) BELOW
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        zoom: 10,
+        center:[-98.493629, 29.424122]
+    });
 
-
+    geocode("San Antonio, TX", MAPBOX_KEY).then(function (result) {
+        console.log(result);
+        map.setCenter(result);
+        map.setZoom(8.7);
+    });
 
 
 
