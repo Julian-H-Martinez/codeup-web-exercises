3517766fec2ec4b93a24df74ae4ad3b4bafb7242
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2021-12-10 10:46:38 -0600

    starting on promises exercise

--- js/promises.js
+++ js/promises.js
@@ -1,2 +1,34 @@
 "use strict";
-console.log('yep i work');
+(()=>{
+    console.log('yep i work');
+    fetch('https://api.github.com/users', {headers: {'Authorization': PROMISE_KEY}})
+        .then(response => {response.json()
+        .then(users => {
+            users.forEach(user =>{
+                console.log(user);
+            })
+        })
+    })
+        .catch(error=> console.log(error));
+
+})()
+const gitHubUsers = fetch('https://api.github.com/users');
+
+
+
+
+
+
+// function getGithubUsernames() {
+//     return fetch('https://api.github.com/users')
+//         .then(response => response.json())
+// }
+//
+// // later on...
+//
+// getGithubUsernames().then( users => {
+//     users.forEach( userObj => {
+//         // do something with each username
+//         console.log(userObj.login);
+//     });
+// }).catch(error => console.error(error));
